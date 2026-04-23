07f0232c5386d29572c5b4e94c40d0920c73ed61
BEN-ESSAHRAOUI-Yassine <ybenessahraoui@gmail.com>
2026-04-23 09:02:53 +0100

    add category seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        
         $this->call([
+            UserSeeder::class,
             CategorySeeder::class,
             ArticleSeeder::class,
         ]);
